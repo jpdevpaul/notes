6ed68ff83f45b791ab9ed7962b050aa88a937542
mewmew <rnd0x00@gmail.com>
2016-11-15 19:11:42 +0100

    ini: Add addresses of themes.cpp for win_109b.

--- ini/diabpsx/source/themes.cpp
+++ ini/diabpsx/source/themes.cpp
@@ -1,155 +1,154 @@
 [TFit_Shrine__Fi]
 psx_jp   = 0x8015BC0C
-win_109b =
+win_109b = 0x0045C870
 mac_109  =
 
 [TFit_Obj5__Fi]
 psx_jp   = 0x8015BEFC
-win_109b =
+win_109b = 0x0045C993
 mac_109  =
 
 [TFit_SkelRoom__Fi]
 psx_jp   = 0x8015C0C0
-win_109b =
+win_109b = 0x0045CA72
 mac_109  =
 
 [TFit_GoatShrine__Fi]
 psx_jp   = 0x8015C170
-win_109b =
+win_109b = 0x0045CAC4
 mac_109  =
 
 [CheckThemeObj3__Fiiii]
 psx_jp   = 0x8015C208
-win_109b =
+win_109b = 0x0045CB09
 mac_109  =
 
 [TFit_Obj3__Fi]
 psx_jp   = 0x8015C354
-win_109b =
+win_109b = 0x0045CB88
 mac_109  =
 
 [CheckThemeReqs__Fi]
 psx_jp   = 0x8015C414
-win_109b =
+win_109b = 0x0045CBE4
 mac_109  =
 
 [SpecialThemeFit__Fii]
 psx_jp   = 0x8015C4E0
-win_109b =
+win_109b = 0x0045CC64
 mac_109  =
 
 [CheckThemeRoom__Fi]
 psx_jp   = 0x8015C6BC
-win_109b =
+win_109b = 0x0045CD9A
 mac_109  =
 
 [InitThemes__Fv]
 psx_jp   = 0x8015C980
-win_109b =
+win_109b = 0x0045CED2
 mac_109  =
 
 [HoldThemeRooms__Fv]
 psx_jp   = 0x8015CCCC
-win_109b =
+win_109b = 0x0045D087
 mac_109  =
 
 [PlaceThemeMonsts__Fii]
 psx_jp   = 0x8015CDB0
-win_109b =
+win_109b = 0x0045D0E1
 mac_109  =
 
 [Theme_Barrel__Fi]
 psx_jp   = 0x8015CF34
-win_109b =
+win_109b = 0x0045D1C2
 mac_109  =
 
 [Theme_Shrine__Fi]
 psx_jp   = 0x8015D090
-win_109b =
+win_109b = 0x0045D29A
 mac_109  =
 
 [Theme_MonstPit__Fi]
 psx_jp   = 0x8015D178
-win_109b =
+win_109b = 0x0045D34D
 mac_109  =
 
 [Theme_SkelRoom__Fi]
 psx_jp   = 0x8015D2BC
-win_109b =
+win_109b = 0x0045D3E6
 mac_109  =
 
 [Theme_Treasure__Fi]
 psx_jp   = 0x8015D5F8
-win_109b =
+win_109b = 0x0045D5BC
 mac_109  =
 
 [Theme_Library__Fi]
 psx_jp   = 0x8015D83C
-win_109b =
+win_109b = 0x0045D707
 mac_109  =
 
 [Theme_Torture__Fi]
 psx_jp   = 0x8015DAC0
-win_109b =
+win_109b = 0x0045D88A
 mac_109  =
 
 [Theme_BloodFountain__Fi]
 psx_jp   = 0x8015DC18
-win_109b =
+win_109b = 0x0045D95D
 mac_109  =
 
 [Theme_Decap__Fi]
 psx_jp   = 0x8015DC8C
-win_109b =
+win_109b = 0x0045D9A3
 mac_109  =
 
 [Theme_PurifyingFountain__Fi]
 psx_jp   = 0x8015DDE4
-win_109b =
+win_109b = 0x0045DA76
 mac_109  =
 
 [Theme_ArmorStand__Fi]
 psx_jp   = 0x8015DE58
-win_109b =
+win_109b = 0x0045DABC
 mac_109  =
 
 [Theme_GoatShrine__Fi]
 psx_jp   = 0x8015DFD4
-win_109b =
+win_109b = 0x0045DBAD
 mac_109  =
 
 [Theme_Cauldron__Fi]
 psx_jp   = 0x8015E108
-win_109b =
+win_109b = 0x0045DC7B
 mac_109  =
 
 [Theme_MurkyFountain__Fi]
 psx_jp   = 0x8015E17C
-win_109b =
+win_109b = 0x0045DCC1
 mac_109  =
 
 [Theme_TearFountain__Fi]
 psx_jp   = 0x8015E1F0
-win_109b =
+win_109b = 0x0045DD07
 mac_109  =
 
 [Theme_BrnCross__Fi]
 psx_jp   = 0x8015E264
-win_109b =
+win_109b = 0x0045DD4D
 mac_109  =
 
 [Theme_WeaponRack__Fi]
 psx_jp   = 0x8015E3C0
-win_109b =
+win_109b = 0x0045DE20
 mac_109  =
 
 [UpdateL4Trans__Fv]
 psx_jp   = 0x8015E53C
-win_109b =
+win_109b = 0x0045DF11
 mac_109  =
 
 [CreateThemeRooms__Fv]
 psx_jp   = 0x8015E598
-win_109b =
+win_109b = 0x0045DF31
 mac_109  =
-
