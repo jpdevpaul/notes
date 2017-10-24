3036ee67426ac755807068a1b27e72d11cdee8a2
mewmew <rnd0x00@gmail.com>
2017-10-24 12:03:13 +0200

    bss: add l1_tile_bit_map notes for drlg_l1.cpp

--- bss/drlg_l1.cpp
+++ bss/drlg_l1.cpp
@@ -1,5 +1,14 @@
 #include "types.h"
 
+// address: 0x525764
+//
+// l1_tile_bit_map represents a tile ID map of twice the size, repeating each
+// tile of the original map in blocks of 4.
+//
+// PSX ref: 0x8013A3B0
+// PSX def: unsigned char L5dungeon[80][80]
+l1_tile_id l1_tile_bit_map[80][80];
+
 // TODO: Define l1_flag enum.
 
 // address: 0x527064
