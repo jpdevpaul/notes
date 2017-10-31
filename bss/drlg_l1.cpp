3f7b84d5b16f5fc9f1203e00b8ff3d5e944ab750
mewmew <rnd0x00@gmail.com>
2017-10-31 14:29:39 +0100

    bss: add notes for drlg_l1.cpp

--- bss/drlg_l1.cpp
+++ bss/drlg_l1.cpp
@@ -18,3 +18,75 @@ l1_tile_id l1_tile_bit_map[80][80];
 // PSX ref: 0x8011C0D8
 // PSX def: unsigned char* mydflags
 uint8_t l1_flag_map[40][40];
+
+// address: 0x5276A4
+//
+// l1_single_player_quest_dun_loaded specifies whether a single player quest DUN
+// has been loaded.
+//
+// PSX ref: 0x8011C0F4
+// PSX def: unsigned char setloadflag
+bool32_t l1_single_player_quest_dun_loaded;
+
+// address: 0x5276A8
+//
+// l1_horiz_room1 specifies whether to generate a horizontal room at position 1
+// in the Cathedral.
+//
+// PSX ref: 0x8011C8D8
+// PSX def: unsigned char HR1
+bool32_t l1_horiz_room1;
+
+// address: 0x5276AC
+//
+// l1_horiz_room2 specifies whether to generate a horizontal room at position 2
+// in the Cathedral.
+//
+// PSX ref: 0x8011C8D9
+// PSX def: unsigned char HR2
+bool32_t l1_horiz_room2;
+
+// address: 0x5276B0
+//
+// l1_horiz_room3 specifies whether to generate a horizontal room at position 3
+// in the Cathedral.
+//
+// PSX ref: 0x8011C8DA
+// PSX def: unsigned char HR3
+bool32_t l1_horiz_room3;
+
+// address: 0x5276B4
+//
+// l1_vert_room1 specifies whether to generate a vertical room at position 1 in
+// the Cathedral.
+//
+// PSX ref: 0x8011C8DB
+// PSX def: unsigned char VR1
+bool32_t l1_vert_room1;
+
+// address: 0x5276B8
+//
+// l1_vert_room2 specifies whether to generate a vertical room at position 2 in
+// the Cathedral.
+//
+// PSX ref: 0x8011C8DC
+// PSX def: unsigned char VR2
+bool32_t l1_vert_room2;
+
+// address: 0x5276BC
+//
+// l1_vert_room3 specifies whether to generate a vertical room at position 3 in
+// the Cathedral.
+//
+// PSX ref: 0x8011C8DD
+// PSX def: unsigned char VR3
+bool32_t l1_vert_room3;
+
+// address: 0x5276C0
+//
+// l1_single_player_quest_dun contains the contents of the single player quest
+// DUN file.
+//
+// PSX ref: 0x8011C0DC
+// PSX def: unsigned char* pSetPiece
+void *l1_single_player_quest_dun;
