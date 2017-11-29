5068cbc63ab1050337a20bcd9e0c9be7f90866b5
mewmew <rnd0x00@gmail.com>
2017-11-29 15:27:16 +0100

    bss: add PSX refs for dead.cpp

--- bss/dead.cpp
+++ bss/dead.cpp
@@ -10,12 +10,18 @@ int unused_4B8CD8;
 // address: 0x4B8CE0
 //
 // deads contains the dead monsters of the current game.
+//
+// PSX ref: 0x800CEB10
+// PSX def: DeadStruct dead[31]
 Dead deads[31];
 
 */
 
 // address: 0x4B92B0
 //
-// dead_num specifies the active dead number (deads array index).
-int dead_num;
-
+// dead_num_stone_curse specifies the dead number (deads array index) of the
+// stone cursed animation.
+//
+// PSX ref: 0x8011B774
+// PSX def: int stonendx
+int dead_num_stone_curse;
