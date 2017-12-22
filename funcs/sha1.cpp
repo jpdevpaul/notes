14f114671da1aabca81cfffab2a55d1f31bf4645
mewmew <rnd0x00@gmail.com>
2017-12-22 19:06:08 +0100

    funcs: add notes for sha1.cpp

--- funcs/sha1.cpp
+++ funcs/sha1.cpp
@@ -16,22 +16,16 @@ void __fastcall sha1_final(int ctx_id, uint8_t (*dst)[20]);
 // context and stores the final message digest in dst.
 void __fastcall sha1(int ctx_id, const uint8_t *data, uint8_t (*dst)[20]);
 
-/* TODO: uncomment when the Sha1Ctx struct has been added. Include sha1.h
-   from types.h? Or create a minimal sha1.h locally for the specific types which
-   are used in Diablo.
-
 // address: 0x456A73
 //
 // sha1_update adds the data to the running hash of the given SHA-1 context.
-void __fastcall sha1_update(Sha1Ctx *ctx, const uint8_t *data, int len);
+void __fastcall sha1_update(SHA1Context *ctx, const uint8_t *data, int len);
 
 // address: 0x456AC4
 //
 // sha1_transform performs a SHA-1 transformation on the 64-byte block of the
 // given SHA-1 context.
-void __fastcall sha1_transform(Sha1Ctx *ctx);
-
-*/
+void __fastcall sha1_transform(SHA1Context *ctx);
 
 // address: 0x456C82
 //
--- types.h
+++ types.h
@@ -5,6 +5,7 @@ typedef int8_t bool8_t;
 typedef int32_t bool32_t;
 
 #include "windows.h"
+#include "sha1.h"
 
 #include "enums.h"
 #include "structs.h"
