4d3795f0b4c955f2ba4512f9902ea7809d157ca7
mewmew <rnd0x00@gmail.com>
2018-01-29 13:49:59 +0100

    funcs: invert doc of restricted write access check return value

--- funcs/restricted.cpp
+++ funcs/restricted.cpp
@@ -7,12 +7,14 @@ bool32_t restricted_is_os_supported();
 
 // address: 0x452885
 //
-// restricted_has_game_win_write_access reports whether the process has write
-// access to the windows directory.
-bool32_t restricted_has_game_win_write_access();
+// restricted_check_game_win_write_access checks whether the process has write
+// access to the windows directory. A return value of false states that the
+// process has write access.
+bool32_t restricted_check_game_win_write_access();
 
 // address: 0x4528F7
 //
-// restricted_has_game_dir_write_access reports whether the process has write
-// access to the game directory.
-bool32_t restricted_has_game_dir_write_access();
+// restricted_check_game_dir_write_access checks whether the process has write
+// access to the game directory. A return value of false states that the process
+// has write access.
+bool32_t restricted_check_game_dir_write_access();
