9cddbfee628bd521e90faefaf4e394d6f71f44c5
mewmew <rnd0x00@gmail.com>
2018-02-15 12:10:02 +0100

    funcs: s/PSX sig/PSX def/ in drlg_l1.cpp

--- funcs/drlg_l1.cpp
+++ funcs/drlg_l1.cpp
@@ -15,7 +15,7 @@ void drlg_l1_reset_maps();
 /// dungeon file.
 ///
 /// PSX ref: 0x8013CF64
-/// PSX sig: void LoadL1Dungeon__FPcii(char *sFileName, int vx, int vy)
+/// PSX def: void LoadL1Dungeon__FPcii(char *sFileName, int vx, int vy)
 void __fastcall drlg_l1_load_dun(char *dun_path, int view_x, int view_y);
 
 /// address: 0x40AF65
@@ -23,7 +23,7 @@ void __fastcall drlg_l1_load_dun(char *dun_path, int view_x, int view_y);
 /// drlg_l1_randomize_stone_floor randomizes floor tiles.
 ///
 /// PSX ref: 0x8013CAC4
-/// PSX sig: void DRLG_L1Floor__Fv()
+/// PSX def: void DRLG_L1Floor__Fv()
 void drlg_l1_randomize_stone_floor();
 
 /// address: 0x40AFB3
@@ -47,7 +47,7 @@ void drlg_l1_init_arches();
 /// drlg_l1_preload_dun loads tile IDs from the given dungeon file.
 ///
 /// PSX ref: 0x8013D138
-/// PSX sig: void LoadPreL1Dungeon__FPcii(char *sFileName, int vx, int vy)
+/// PSX def: void LoadPreL1Dungeon__FPcii(char *sFileName, int vx, int vy)
 void __fastcall drlg_l1_preload_dun(char *dun_path, int view_x, int view_y);
 
 /// address: 0x40B229
@@ -56,7 +56,7 @@ void __fastcall drlg_l1_preload_dun(char *dun_path, int view_x, int view_y);
 /// given seed and level entry.
 ///
 /// PSX ref: 0x80140E64
-/// PSX sig: void CreateL5Dungeon__FUii(unsigned int rseed, int entry)
+/// PSX def: void CreateL5Dungeon__FUii(unsigned int rseed, int entry)
 void __fastcall drlg_l1_create_dungeon(uint32_t seed, int entry);
 
 /// address: 0x40B276
@@ -65,7 +65,7 @@ void __fastcall drlg_l1_create_dungeon(uint32_t seed, int entry);
 /// quest level.
 ///
 /// PSX ref: 0x8013CDA0
-/// PSX sig: void DRLG_LoadL1SP__Fv()
+/// PSX def: void DRLG_LoadL1SP__Fv()
 void drlg_l1_load_quest_dun();
 
 /// address: 0x40B2F4
@@ -73,7 +73,7 @@ void drlg_l1_load_quest_dun();
 /// drlg_l1_free_quest_dun frees the dungeon file of the active quest level.
 ///
 /// PSX ref: 0x8013CE7C
-/// PSX sig: void DRLG_FreeL1SP__Fv()
+/// PSX def: void DRLG_FreeL1SP__Fv()
 void drlg_l1_free_quest_dun();
 
 /// address: 0x40B306
@@ -82,7 +82,7 @@ void drlg_l1_free_quest_dun();
 /// level entry.
 ///
 /// PSX ref: 0x80140930
-/// PSX sig: void DRLG_L5__Fi(int entry)
+/// PSX def: void DRLG_L5__Fi(int entry)
 void __fastcall drlg_l1_generate_dungeon(int entry);
 
 /// address: 0x40B56F
@@ -90,7 +90,7 @@ void __fastcall drlg_l1_generate_dungeon(int entry);
 /// drlg_l1_place_door places a door at the given coordinate.
 ///
 /// PSX ref: 0x8013BCB0
-/// PSX sig: void DRLG_PlaceDoor__Fii(int x, int y)
+/// PSX def: void DRLG_PlaceDoor__Fii(int x, int y)
 void __fastcall drlg_l1_place_door(int tx, int ty);
 
 /// address: 0x40B699
@@ -98,7 +98,7 @@ void __fastcall drlg_l1_place_door(int tx, int ty);
 /// drlg_l1_init_shadows initializes arch and bar shadows.
 ///
 /// PSX ref: 0x8013C190
-/// PSX sig: void DRLG_L1Shadows__Fv()
+/// PSX def: void DRLG_L1Shadows__Fv()
 void drlg_l1_init_shadows();
 
 /// address: 0x40B881
@@ -106,7 +106,7 @@ void drlg_l1_init_shadows();
 /// drlg_l1_place_miniset places the given miniset of tile IDs.
 ///
 /// PSX ref: 0x8013C5A0
-/// PSX sig: int DRLG_PlaceMiniSet__FPCUciiiiiii(unsigned char *miniset, int tmin, int tmax, int cx, int cy, int setview, int noquad, int ldir)
+/// PSX def: int DRLG_PlaceMiniSet__FPCUciiiiiii(unsigned char *miniset, int tmin, int tmax, int cx, int cy, int setview, int noquad, int ldir)
 int __fastcall drlg_l1_place_miniset(uint8_t *miniset, int tmin, int tmax, int cx, int cy, bool32_t set_view, int noquad, int ldir);
 
 /// address: 0x40BAF6
@@ -138,7 +138,7 @@ void drlg_l1_generate_first_room();
 /// drlg_l1_add_room adds walls for a room at the given area.
 ///
 /// PSX ref: 0x8013D3CC
-/// PSX sig: void L5drawRoom__Fiiii(int x, int y, int w, int h)
+/// PSX def: void L5drawRoom__Fiiii(int x, int y, int w, int h)
 void __fastcall drlg_l1_add_room(int tx, int ty, int tw, int th);
 
 /// address: 0x40BD9D
@@ -147,7 +147,7 @@ void __fastcall drlg_l1_add_room(int tx, int ty, int tw, int th);
 /// specified coordinates.
 ///
 /// PSX ref: 0x8013D4CC
-/// PSX sig: void L5roomGen__Fiiiii(int x, int y, int w, int h, int dir)
+/// PSX def: void L5roomGen__Fiiiii(int x, int y, int w, int h, int dir)
 void __fastcall drlg_l1_generate_room(int tx, int ty, int tw, int th, bool32_t dir_horiz);
 
 /// address: 0x40BFA4
@@ -155,7 +155,7 @@ void __fastcall drlg_l1_generate_room(int tx, int ty, int tw, int th, bool32_t d
 /// drlg_l1_is_area_empty reports whether the given area is empty.
 ///
 /// PSX ref: 0x8013D438
-/// PSX sig: unsigned char L5checkRoom__Fiiii(int x, int y, int width, int height)
+/// PSX def: unsigned char L5checkRoom__Fiiii(int x, int y, int width, int height)
 bool32_t __fastcall drlg_l1_is_area_empty(int tx, int ty, int tw, int th);
 
 /// address: 0x40C008
@@ -163,7 +163,7 @@ bool32_t __fastcall drlg_l1_is_area_empty(int tx, int ty, int tw, int th);
 /// drlg_l1_get_area returns the number of walls on the map.
 ///
 /// PSX ref: 0x8013DB9C
-/// PSX sig: long L5GetArea__Fv()
+/// PSX def: long L5GetArea__Fv()
 int drlg_l1_get_area();
 
 /// address: 0x40C02A
@@ -197,7 +197,7 @@ void drlg_l1_add_wall();
 /// that fit at the given coordinate.
 ///
 /// PSX ref: 0x8013DD70
-/// PSX sig: int L5HWallOk__Fii(int i, int j)
+/// PSX def: int L5HWallOk__Fii(int i, int j)
 int __fastcall drlg_l1_get_horiz_wall_space(int tx, int ty);
 
 /// address: 0x40C2DC
@@ -206,7 +206,7 @@ int __fastcall drlg_l1_get_horiz_wall_space(int tx, int ty);
 /// fit at the given coordinate.
 ///
 /// PSX ref: 0x8013DEAC
-/// PSX sig: int L5VWallOk__Fii(int i, int j)
+/// PSX def: int L5VWallOk__Fii(int i, int j)
 int __fastcall drlg_l1_get_vert_wall_space(int tx, int ty);
 
 /// address: 0x40C35B
@@ -214,7 +214,7 @@ int __fastcall drlg_l1_get_vert_wall_space(int tx, int ty);
 /// drlg_l1_add_horiz_wall adds a horizontal wall based on the given tile ID.
 ///
 /// PSX ref: 0x8013DFF4
-/// PSX sig: void L5HorizWall__Fiici(int i, int j, char p, int dx)
+/// PSX def: void L5HorizWall__Fiici(int i, int j, char p, int dx)
 void __fastcall drlg_l1_add_horiz_wall(int tx, int ty, l1_tile_id tile_id, int tw);
 
 /// address: 0x40C449
@@ -222,7 +222,7 @@ void __fastcall drlg_l1_add_horiz_wall(int tx, int ty, l1_tile_id tile_id, int t
 /// drlg_l1_add_vert_wall adds a vertical wall based on the given tile ID.
 ///
 /// PSX ref: 0x8013E22C
-/// PSX sig: void L5VertWall__Fiici(int i, int j, char p, int dy)
+/// PSX def: void L5VertWall__Fiici(int i, int j, char p, int dy)
 void __fastcall drlg_l1_add_vert_wall(int tx, int ty, l1_tile_id tile_id, int th);
 
 /// address: 0x40C551
@@ -255,7 +255,7 @@ void drlg_l1_generate_chambers();
 /// columns on the specified sides.
 ///
 /// PSX ref: 0x8013E6B4
-/// PSX sig: void DRLG_L5GChamber__Fiiiiii(int sx, int sy, int topflag, int bottomflag, int leftflag, int rightflag)
+/// PSX def: void DRLG_L5GChamber__Fiiiiii(int sx, int sy, int topflag, int bottomflag, int leftflag, int rightflag)
 void __fastcall drlg_l1_generate_chamber(int tx, int ty, bool32_t top_right, bool32_t bottom_left, bool32_t top_left, bool32_t bottom_right);
 
 /// address: 0x40CEC7
@@ -263,7 +263,7 @@ void __fastcall drlg_l1_generate_chamber(int tx, int ty, bool32_t top_right, boo
 /// drlg_l1_generate_hall generates a hall of columns and arches.
 ///
 /// PSX ref: 0x8013E974
-/// PSX sig: void DRLG_L5GHall__Fiiii(int x1, int y1, int x2, int y2)
+/// PSX def: void DRLG_L5GHall__Fiiii(int x1, int y1, int x2, int y2)
 void __fastcall drlg_l1_generate_hall(int tx_start, int ty_start, int tx_end, int ty_end);
 
 /// address: 0x40CF17
@@ -272,7 +272,7 @@ void __fastcall drlg_l1_generate_hall(int tx_start, int ty_start, int tx_end, in
 /// dungeon file.
 ///
 /// PSX ref: 0x8013F4F8
-/// PSX sig: void DRLG_L5SetRoom__Fii(int rx1, int ry1)
+/// PSX def: void DRLG_L5SetRoom__Fii(int rx1, int ry1)
 void __fastcall drlg_l1_init_quest_dun(int tx, int ty);
 
 /// address: 0x40CF9C
@@ -289,7 +289,7 @@ void drlg_l1_floor_transparency();
 /// concealing walls.
 ///
 /// PSX ref: 0x8013FCE4
-/// PSX sig: void DRLG_L5FTVR__Fiiiii(int i, int j, int x, int y, int d)
+/// PSX def: void DRLG_L5FTVR__Fiiiii(int i, int j, int x, int y, int d)
 void __fastcall drlg_l1_floor_transparency_recursive(int tx, int ty, int x, int y, int direction);
 
 /// address: 0x40D1FB
@@ -306,7 +306,7 @@ void drlg_l1_fix_transparency();
 /// drlg_l1_fix_dirt fixes dirt tile IDs after dungeon generation.
 ///
 /// PSX ref: 0x801406A8
-/// PSX sig: void DRLG_L5DirtFix__Fv()
+/// PSX def: void DRLG_L5DirtFix__Fv()
 void drlg_l1_fix_dirt();
 
 /// address: 0x40D2EF
