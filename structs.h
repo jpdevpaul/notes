2f063d89910df5a275e95513ee6294f27485c0f7
Sergey Semushin <predelnik@gmail.com>
2018-02-24 21:32:35 +0300

    structs: Item: fix size for is_identified member.

--- structs.h
+++ structs.h
@@ -922,7 +922,7 @@ typedef struct {
 	item_drop_state drop_state;
 	/// offset 0034 (4 bytes)
 	bool32_t draw_quest_item;
-	/// offset 0038 (1 byte)
+	/// offset 0038 (4 bytes)
 	bool32_t is_identified;
 	/// offset 003C (1 byte)
 	item_quality quality;
