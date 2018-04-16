36c4b0691f559f9e9f45e8a8b9b80bf5b947e765
mewmew <rnd0x00@gmail.com>
2018-04-17 00:32:02 +0200

    bss: add automap_discovered_from_dlvl documentation for msg.cpp

--- bss/msg.cpp
+++ bss/msg.cpp
@@ -11,6 +11,12 @@ float msg_cpp_init_value;
 /// each dungeon level to be synchronized with connected peers.
 DeltaLevel delta_levels[17];
 
+/// address: 0x66F728
+///
+/// automap_discovered_from_dlvl maps tracks the explored areas of the map for
+/// each dungeon level.
+bool8_t automap_discovered_from_dlvl[17][40][40];
+
 /// address: 0x676168
 ///
 /// portals contains the town portals of the current game.
--- funcs/path.cpp
+++ funcs/path.cpp
@@ -2,7 +2,9 @@
 
 /// address: 0x4493D4
 ///
-/// TODO: add documentation.
+/// path_find locates a path from the start to the goal coordinates. It returns
+/// the number of steps in the path, and stores the path in the steps parameter.
+int __fastcall path_find(bool32_t (__fastcall *is_valid_step)(int player_or_monster_num, int x, int y), int player_or_monster_num, int start_x, int start_y, int target_x, int target_y, int8_t *steps);
 
 /// address: 0x4494D3
 ///
@@ -34,11 +36,15 @@ int __fastcall path_get_h_cost(int start_x, int start_y, int target_x, int targe
 
 /// address: 0x44979A
 ///
-/// TODO: add documentation.
+/// path_get_node1 returns the path node for the given coordinate, starting
+/// traversal from the first node.
+PathNode *__fastcall path_get_node1(int x, int y);
 
 /// address: 0x4497B3
 ///
-/// TODO: add documentation.
+/// path_get_node2 returns the path node for the given coordinate, starting
+/// traversal from the second node.
+PathNode *__fastcall path_get_node2(int x, int y);
 
 /// address: 0x4497CC
 ///
