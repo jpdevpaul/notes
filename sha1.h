a0ee57c8b5e416b0d338fb5369d9d41348fff070
mewmew <rnd0x00@gmail.com>
2018-04-24 22:57:35 +0200

    add include header guard to sha1.h

--- sha1.h
+++ sha1.h
@@ -1,6 +1,11 @@
+#ifndef SHA1_H
+#define SHA1_H
+
 /// SHA1Context holds the context of a SHA-1 running hashsum.
 typedef struct {
 	uint32_t state[5];
 	int32_t count[2];
 	uint8_t buffer[64];
 } SHA1Context;
+
+#endif // SHA1_H
