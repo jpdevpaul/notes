7706d21aeee3a8ca4d19a8d7eead162512b9ccd6
Robin Eklind <rnd0x00@gmail.com>
2019-08-07 16:17:10 +0200

    rdata: use PSX names for cursor.h

--- rdata/cursor.h
+++ rdata/cursor.h
@@ -2,18 +2,16 @@
 
 /// address: 0x479658
 ///
-/// objcurs_frame_width_from_frame maps from objcurs.cel frame number to frame
-/// width.
+/// InvItemWidth maps from objcurs.cel frame number to frame width.
 ///
 /// PSX ref: 0x8010D518
 /// PSX def: unsigned char InvItemWidth[180]
-extern int32_t objcurs_frame_width_from_frame[180];
+extern uint32_t InvItemWidth[180]; // alias: objcurs_frame_width_from_frame
 
 /// address: 0x479928
 ///
-/// objcurs_frame_height_from_frame maps from objcurs.cel frame number to frame
-/// height.
+/// InvItemHeight maps from objcurs.cel frame number to frame height.
 ///
 /// PSX ref: 0x8010D5CC
 /// PSX def: unsigned char InvItemHeight[180]
-extern int32_t objcurs_frame_height_from_frame[180];
+extern uint32_t InvItemHeight[180]; // alias: objcurs_frame_height_from_frame
