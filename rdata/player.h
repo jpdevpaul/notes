62863faea324efb1f9c2e8e00ceb486b545fbeed
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 22:28:43 +0200

    rdata: use PSX names for player.h
    Updates #253.

--- rdata/player.h
+++ rdata/player.h
@@ -5,18 +5,30 @@ extern float player_inf;
 
 /// address: 0x47F208
 ///
-/// player_armors maps from armor animation to letter used in graphic files.
-extern char player_armors[3];
+/// ArmourChar maps from armor animation to letter used in graphic files.
+///
+/// PSX ref (SLPS-01416): 0x8011B008
+/// PSX def: char ArmourChar[4]
+// Note: char[3] is enough, though PSX details as char[4].
+extern char ArmourChar[4]; // alias: player_armors
 
 /// address: 0x47F20C
 ///
-/// player_weapons maps from weapon animation to letter used in graphic files.
-extern char player_weapons[9];
+/// WepChar maps from weapon animation to letter used in graphic files.
+///
+/// PSX ref (SLPS-01416): 0x80110B68
+/// PSX def: char WepChar[10]
+// Note: char[9] is enough, though PSX details as char[10].
+extern char WepChar[10]; // alias: player_weapons
 
 /// address: 0x47F218
 ///
-/// player_classes maps from player class to letter used in graphic files.
-extern char player_classes[3];
+/// CharChar maps from player class to letter used in graphic files.
+///
+/// PSX ref (SLPS-01416): 0x8011B00C
+/// PSX def: char CharChar[4]
+// Note: char[3] is enough, though PSX details as char[4].
+extern char CharChar[4]; // alias: player_classes
 
 /// address: 0x47F220
 ///
