f609ea371d0768a6927d5e8ef931d23243d73387
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 09:41:00 +0200

    rdata: use PSX names in effects.h
    Note, the PSX sym file does not seem to include the
    original name for monster_action_sounds. However, it
    does include the names for very similar global
    variables used for players rather than monsters, so
    from these we may infer a similar name for
    monster_action_sounds.
    
    In particular, ArmourChar, WepChar and CharChar are
    used for player graphics. So, following the same naming
    convention we arrive at MonstSndChar.
    
    Updates #253.

--- rdata/effects.h
+++ rdata/effects.h
@@ -5,6 +5,6 @@ extern float effects_inf;
 
 /// address: 0x47A46C
 ///
-/// monster_action_sounds specifies the characters used in the file name of
-/// monster action sound files (a=attack, h=hit, d=die, s=special attack).
-extern char monster_action_sounds[4];
+/// MonstSndChar specifies the characters used in the file name of monster
+/// action sound files (a=attack, h=hit, d=die, s=special attack).
+extern char MonstSndChar[4]; // alias: monster_action_sounds
