f7dd3f3d1337721bddb8ae1b685dbb2fbec690b1
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 22:39:52 +0200

    rdata: use PSX names in towners.h
    Updates #253.

--- rdata/towners.h
+++ rdata/towners.h
@@ -2,10 +2,12 @@
 
 /// address: 0x4802AC
 ///
-/// cow_sfx_id_from_class maps from active cow sound effect index and player
-/// class to sound effect ID for interacting with cows in Tristram.
+/// snSFX maps from active cow sound effect index and player class to sound
+/// effect ID for interacting with cows in Tristram.
 ///
-/// References:
-///    * https://github.com/sanctuary/notes/blob/master/data/towners.cpp#cow_active_sfx_id
-///    * https://github.com/sanctuary/notes/blob/master/enums.h#player_class
-extern sfx_id cow_sfx_id_from_class[3][3];
+/// ref: enum cow_active_sfx_id
+/// ref: enum player_class
+///
+/// PSX ref (SLPS-01416): 0x801112CC
+/// PSX def: int snSFX[3][3]
+extern sfx_id snSFX[3][3]; // alias: cow_sfx_id_from_class
