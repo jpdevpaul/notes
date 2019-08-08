a295a130faaa84ebae7617911d1c225d4c9d760b
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 21:44:30 +0200

    rdata: use PSX names in items.h

--- rdata/items.h
+++ rdata/items.h
@@ -2,24 +2,24 @@
 
 /// address: 0x47B0A8
 ///
-/// item_prefix_data contains the data related to each item prefix.
+/// PL_Prefix contains the data related to each item prefix.
 ///
-/// PSX ref: 0x80112744
+/// PSX ref (SLPS-01416): 0x80112744
 /// PSX def: PLStruct PL_Prefix[84]
-extern ItemAffixData item_prefix_data[84];
+extern ItemAffixData PL_Prefix[84]; // alias: item_prefix_data
 
 /// address: 0x47C068
 ///
-/// item_suffix_data contains the data related to each item suffix.
+/// PL_Suffix contains the data related to each item suffix.
 ///
-/// PSX ref: 0x80113464
+/// PSX ref (SLPS-01416): 0x80113464
 /// PSX def: PLStruct PL_Suffix[96]
-extern ItemAffixData item_suffix_data[96];
+extern ItemAffixData PL_Suffix[96]; // alias: item_suffix_data
 
 /// address: 0x47D268
 ///
-/// unique_item_data contains the data related to each unique item ID.
+/// UniqueItemList contains the data related to each unique item ID.
 ///
-/// PSX ref: 0x80114364
+/// PSX ref (SLPS-01416): 0x80114364
 /// PSX def: UItemStruct UniqueItemList[91]
-extern UniqueItemData unique_item_data[91];
+extern UniqueItemData UniqueItemList[91]; // alias: unique_item_data
