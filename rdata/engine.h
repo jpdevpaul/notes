a479cc56b8fcb7db1a88ac487a10147c99033963
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 10:00:22 +0200

    rdata: use PSX names for globals in engine.h
    Note, neither rand_increment nor rand_multiplier are
    present in the PSX debug info. To keep the names for
    these variables consistent with the naming convention
    used for sglGameSeed, SeedCount, SetRndSeed and
    GetRndSeed (which are part of the PSX debug info), we
    rename them to RndInc and RndMult, respectively.
    
    Updates #253.

--- rdata/engine.h
+++ rdata/engine.h
@@ -7,20 +7,18 @@ extern float engine_inf;
 
 /// address: 0x47A478
 ///
-/// rand_increment specifies the increment used in the Borland C/C++ pseudo-
-/// random number generator algorithm.
+/// RndInc specifies the increment used in the Borland C/C++ pseudo- random
+/// number generator algorithm.
 ///
+///    seed = RndMult*seed + RndInc
 ///    seed = 0x15A4E35*seed + 1
-///
-/// Note, this global variable is unused.
-extern int32_t rand_increment;
+extern const int32_t RndInc; // alias: rand_increment
 
 /// address: 0x47A47C
 ///
-/// rand_multiplier specifies the multiplier used in the Borland C/C++ pseudo-
-/// random number generator algorithm.
+/// RndMult specifies the multiplier used in the Borland C/C++ pseudo- random
+/// number generator algorithm.
 ///
-///   seed = 0x15A4E35*seed + 1
-///
-/// Note, this global variable is unused.
-extern int32_t rand_multiplier;
+///    seed = RndMult*seed + RndInc
+///    seed = 0x15A4E35*seed + 1
+extern const int32_t RndMult; // alias: rand_multiplier
