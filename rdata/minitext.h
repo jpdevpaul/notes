b986458518d7881a328af9d731a1e562361e1dc7
Robin Eklind <rnd0x00@gmail.com>
2019-08-08 21:48:07 +0200

    rdata: use PSX names for globals of minitext.h
    Note, the mfontframe and mfontkern globals don't have
    PSX debug info, however, they correspond to the
    fontframe and fontkern variables of control.h, but
    apply to medium instead of small fonts. Thus the names
    are derived as such. Also, the PSX debug info does
    include global variables of the format FontTab,
    LFontTab, MFontTab. As such, M and L prefixes are used
    for medium and large fonts, and a missing prefix is
    used for small fonts.

--- rdata/minitext.h
+++ rdata/minitext.h
@@ -2,13 +2,12 @@
 
 /// address: 0x47F078
 ///
-/// medtexts_frame_from_font_index maps from font index to medtexts.cel frame
-/// number.
-extern int8_t medtexts_frame_from_font_index[127];
+/// mfontframe maps from font index to medtexts.cel frame number.
+extern uint8_t mfontframe[127]; // alias: medtexts_frame_from_font_index
 
 /// address: 0x47F0F8
 ///
-/// medtexts_character_width_from_frame maps from medtexts.cel frame number to
-/// character width. Note, the character width may be distinct from the frame
-/// width, which is 22 for every medtexts.cel frame.
-extern int8_t medtexts_character_width_from_frame[56];
+/// mfontkern maps from medtexts.cel frame number to character width. Note, the
+/// character width may be distinct from the frame width, which is 22 for every
+/// medtexts.cel frame.
+extern uint8_t mfontkern[56]; // alias: medtexts_character_width_from_frame
