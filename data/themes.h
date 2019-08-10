5a37ac739648f4d5b9a72137a0fa971a67a43c1d
Robin Eklind <rnd0x00@gmail.com>
2019-08-10 14:25:32 +0200

    data: use PSX names for themes.h

--- data/themes.h
+++ data/themes.h
@@ -2,26 +2,41 @@
 
 /// address: 0x4B2584
 ///
-/// special_theme_ids specifies the set of special theme IDs from which one will
-/// be selected at random.
-extern theme_id special_theme_ids[4];
+/// ThemeGood specifies the set of special theme IDs from which one will be
+/// selected at random.
+///
+/// PSX ref (SLPS-01416): 0x80102728
+/// PSX def: int ThemeGood[4]
+extern theme_id ThemeGood[4]; // alias: special_theme_ids
 
 /// address: 0x4B2594
 ///
 /// x_delta_5x5 specifies a 5x5 area to fit theme objects.
-extern int x_delta_5x5[25];
+///
+/// PSX ref (SLPS-01416): 0x80102738
+/// PSX def: int trm5x[25]
+extern int32_t trm5x[25]; // alias: x_delta_5x5
 
 /// address: 0x4B25F8
 ///
 /// y_delta_5x5 specifies a 5x5 area to fit theme objects.
-extern int y_delta_5x5[25];
+///
+/// PSX ref (SLPS-01416): 0x8010279C
+/// PSX def: int trm5y[25]
+extern int32_t trm5y[25]; // alias: y_delta_5x5
 
 /// address: 0x4B265C
 ///
 /// x_delta_3x3 specifies a 3x3 area to fit theme objects.
-extern int x_delta_3x3[9];
+///
+/// PSX ref (SLPS-01416): 0x80102800
+/// PSX def: int trm3x[9]
+extern int32_t trm3x[9]; // alias: x_delta_3x3
 
 /// address: 0x4B2680
 ///
 /// y_delta_3x3 specifies a 3x3 area to fit theme objects.
-extern int y_delta_3x3[9];
+///
+/// PSX ref (SLPS-01416): 0x80102824
+/// PSX def: int trm3y[9]
+extern int32_t trm3y[9]; // alias: y_delta_3x3
