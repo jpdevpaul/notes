973be6d644f6eb67bd318e16b5c813a53d4ed7c2
Robin Eklind <rnd0x00@gmail.com>
2019-08-10 12:08:28 +0200

    data: use PSX names for portal.h

--- data/portal.h
+++ data/portal.h
@@ -1,11 +1,17 @@
 /// address: 0x4A1ABC
 ///
-/// portal_town_y_from_player_num maps from player number to Y-coordinate of the
-/// town portal position in Tristram.
-extern int portal_town_y_from_player_num[4];
+/// WarpDropX maps from player number to Y-coordinate of the town portal
+/// position in Tristram.
+///
+/// PSX ref (SLPS-01416): 0x800E3BCC
+/// PSX def: int WarpDropX[4]
+extern int32_t WarpDropX[4]; // alias: portal_town_x_from_player_num
 
 /// address: 0x4A1ACC
 ///
-/// portal_town_x_from_player_num maps from player number to X-coordinate of the
-/// town portal position in Tristram.
-extern int portal_town_x_from_player_num[4];
+/// WarpDropY maps from player number to X-coordinate of the town portal
+/// position in Tristram.
+///
+/// PSX ref (SLPS-01416): 0x800E3BDC
+/// PSX def: int WarpDropY[4]
+extern int32_t WarpDropY[4]; // alias: portal_town_y_from_player_num
