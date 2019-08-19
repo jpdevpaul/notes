76192935b41aadbcfd514225c991753a44d5c055
Robin Eklind <rnd0x00@gmail.com>
2019-08-19 17:04:02 +0200

    bss: use PSX names in drlg_l1.h

--- bss/drlg_l1.h
+++ bss/drlg_l1.h
@@ -2,91 +2,89 @@
 
 /// address: 0x525764
 ///
-/// l1_tile_bit_map represents a tile ID map of twice the size, repeating each
-/// tile of the original map in blocks of 4.
+/// L5dungeon represents a tile ID map of twice the size, repeating each tile of
+/// the original map in blocks of 4.
 ///
 /// PSX ref: 0x8013A3B0
 /// PSX def: unsigned char L5dungeon[80][80]
-extern l1_tile_id l1_tile_bit_map[80][80];
+extern l1_tile_id L5dungeon[80][80]; // alias: l1_tile_bit_map
 
 /// TODO: Define l1_flag enum.
 
 /// address: 0x527064
 ///
-/// l1_flag_map contains flags used for dungeon generation of the Cathedral.
+/// mydflags contains flags used for dungeon generation of the Cathedral.
 ///
 /// PSX ref: 0x8011C0D8
 /// PSX def: unsigned char* mydflags
-extern uint8_t l1_flag_map[40][40];
+extern uint8_t mydflags[40][40]; // alias: l1_flag_map
 
 /// address: 0x5276A4
 ///
-/// l1_single_player_quest_dun_loaded specifies whether a single player quest DUN
-/// has been loaded.
+/// setloadflag specifies whether a single player quest DUN has been loaded.
 ///
 /// PSX ref: 0x8011C0F4
 /// PSX def: unsigned char setloadflag
-extern bool32_t l1_single_player_quest_dun_loaded;
+extern bool32_t setloadflag; // alias: l1_single_player_quest_dun_loaded
 
 /// address: 0x5276A8
 ///
-/// l1_horiz_room1 specifies whether to generate a horizontal room at position 1
-/// in the Cathedral.
+/// HR1 specifies whether to generate a horizontal room at position 1 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8D8
 /// PSX def: unsigned char HR1
-extern bool32_t l1_horiz_room1;
+extern bool32_t HR1; // alias: l1_horiz_room1
 
 /// address: 0x5276AC
 ///
-/// l1_horiz_room2 specifies whether to generate a horizontal room at position 2
-/// in the Cathedral.
+/// HR2 specifies whether to generate a horizontal room at position 2 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8D9
 /// PSX def: unsigned char HR2
-extern bool32_t l1_horiz_room2;
+extern bool32_t HR2; // alias: l1_horiz_room2
 
 /// address: 0x5276B0
 ///
-/// l1_horiz_room3 specifies whether to generate a horizontal room at position 3
-/// in the Cathedral.
+/// HR3 specifies whether to generate a horizontal room at position 3 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8DA
 /// PSX def: unsigned char HR3
-extern bool32_t l1_horiz_room3;
+extern bool32_t HR3; // alias: l1_horiz_room3
 
 /// address: 0x5276B4
 ///
-/// l1_vert_room1 specifies whether to generate a vertical room at position 1 in
-/// the Cathedral.
+/// VR1 specifies whether to generate a vertical room at position 1 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8DB
 /// PSX def: unsigned char VR1
-extern bool32_t l1_vert_room1;
+extern bool32_t VR1; // alias: l1_vert_room1
 
 /// address: 0x5276B8
 ///
-/// l1_vert_room2 specifies whether to generate a vertical room at position 2 in
-/// the Cathedral.
+/// VR2 specifies whether to generate a vertical room at position 2 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8DC
 /// PSX def: unsigned char VR2
-extern bool32_t l1_vert_room2;
+extern bool32_t VR2; // alias: l1_vert_room2
 
 /// address: 0x5276BC
 ///
-/// l1_vert_room3 specifies whether to generate a vertical room at position 3 in
-/// the Cathedral.
+/// VR3 specifies whether to generate a vertical room at position 3 in the
+/// Cathedral.
 ///
 /// PSX ref: 0x8011C8DD
 /// PSX def: unsigned char VR3
-extern bool32_t l1_vert_room3;
+extern bool32_t VR3; // alias: l1_vert_room3
 
 /// address: 0x5276C0
 ///
-/// l1_single_player_quest_dun contains the contents of the single player quest
-/// DUN file.
+/// pSetPiece contains the contents of the single player quest DUN file.
 ///
 /// PSX ref: 0x8011C0DC
 /// PSX def: unsigned char* pSetPiece
-extern void *l1_single_player_quest_dun;
+extern void *pSetPiece; // alias: l1_single_player_quest_dun
