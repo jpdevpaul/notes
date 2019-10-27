38e54062d337947deef2a0be451c794c3b4a0acf
Robin Eklind <rnd0x00@gmail.com>
2019-10-27 16:17:56 +0100

    funcs: sync appfat names with Devilution

--- funcs/appfat.h
+++ funcs/appfat.h
@@ -2,109 +2,103 @@
 
 /// address: 0x40102A
 ///
-/// appfat_get_error_string returns a formatted error message based on the given
-/// error code.
-char * __fastcall appfat_get_error_string(uint32_t error_code);
+/// GetErrorStr returns a formatted error message based on the given error
+/// code.
+char * __fastcall GetErrorStr(DWORD error_code);
 
 /// address: 0x4010CE
 ///
-/// appfat_get_DD_error_string stores a formatted error message in error_buf
-/// based on the given DirectDraw error code.
-void __fastcall appfat_get_DD_error_string(uint32_t error_code, char *error_buf, int error_buf_len);
+/// TraceErrorDD stores a formatted error message in error_buf based on the
+/// given DirectDraw error code.
+void __fastcall TraceErrorDD(HRESULT hError, char *pszBuffer, DWORD dwMaxChars);
 
 /// address: 0x401831
 ///
-/// appfat_get_DS_error_string stores a formatted error message in error_buf
-/// based on the given DirectSound error code.
-void __fastcall appfat_get_DS_error_string(uint32_t error_code, char *error_buf, int error_buf_len);
+/// TraceErrorDS stores a formatted error message in error_buf based on the
+/// given DirectSound error code.
+void __fastcall TraceErrorDS(HRESULT hError, char *pszBuffer, DWORD dwMaxChars);
 
 /// address: 0x40193A
 ///
-/// appfat_get_last_error returns a formatted error message of the last error.
-char * appfat_get_last_error();
+/// TraceLastError returns a formatted error message of the last error.
+char * TraceLastError();
 
 /// address: 0x401947
 ///
-/// appfat_terminate_message_box terminates the game and displays an error
-/// message box.
-void appfat_terminate_message_box(char *format, ...);
+/// app_fatal terminates the game and displays an error message box.
+void __cdecl app_fatal(const char *pszFmt, ...);
 
 /// address: 0x401975
 ///
-/// appfat_message_box displays an error message box based on the given format
-/// string and variable argument list.
-void __fastcall appfat_message_box(char *format, va_list va);
+/// MsgBox displays an error message box based on the given format string and
+/// variable argument list.
+void __fastcall MsgBox(const char *pszFmt, va_list va);
 
 /// address: 0x4019C7
 ///
-/// appfat_cleanup cleans up after a fatal application error.
-void appfat_cleanup();
+/// FreeDlg cleans up after a fatal application error.
+void FreeDlg();
 
 /// address: 0x401A30
 ///
-/// appfat_draw_message_box renders an in-game message box with the given text
-/// on screen.
-void appfat_draw_message_box(char *format, ...);
+/// DrawDlg renders an in-game message box with the given text on screen.
+void __cdecl DrawDlg(char *pszFmt, ...);
 
 /// address: 0x401A65
 ///
-/// appfat_DD_assert terminates the game with a DirectDraw assertion message box.
-void __fastcall appfat_DD_assert(uint32_t error_code, int log_line_nr, char *log_file_path);
+/// DDErrMsg terminates the game with a DirectDraw assertion message box.
+void __fastcall DDErrMsg(DWORD error_code, int log_line_nr, char *log_file_path);
 
 /// address: 0x401A88
 ///
-/// appfat_DS_assert terminates the game with a DirectSound assertion message
-/// box.
-void __fastcall appfat_DS_assert(uint32_t error_code, int log_line_nr, char *log_file_path);
+/// DSErrMsg terminates the game with a DirectSound assertion message box.
+void __fastcall DSErrMsg(DWORD error_code, int log_line_nr, char *log_file_path);
 
 /// address: 0x401AAB
 ///
-/// appfat_centre_dialog_box centres the given dialog box.
-void __fastcall appfat_centre_dialog_box(HWND hDlg);
+/// center_window centres the given dialog box.
+void __fastcall center_window(HWND hDlg);
 
 /// address: 0x401B3D
 ///
-/// appfat_terminate_dialog_box terminates the game and displays an error dialog
-/// box based on the given template id.
-void __fastcall appfat_terminate_dialog_box(int template_id, uint32_t error_code, char *log_file_path, int log_line_nr);
+/// ErrDlg terminates the game and displays an error dialog box based on the
+/// given template id.
+void __fastcall ErrDlg(int template_id, DWORD error_code, char *log_file_path, int log_line_nr);
 
 /// address: 0x401BCA
 ///
-/// appfat_dialog_func is a callback function which processes messages sent to
-/// the given dialog box.
-bool32_t __stdcall appfat_dialog_func(HWND hDlg, UINT uMsg, WPARAM wParam, char *text);
+/// FuncDlg is a callback function which processes messages sent to the given
+/// dialog box.
+BOOL __stdcall FuncDlg(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM text);
 
 /// address: 0x401C0F
 ///
-/// appfat_set_dialog_text sets the text of the given dialog.
-void __fastcall appfat_set_dialog_text(HWND hDlg, char *text);
+/// TextDlg sets the text of the given dialog.
+void __fastcall TextDlg(HWND hDlg, char *text);
 
 /// address: 0x401C2E
 ///
-/// appfat_dialog_box displays an error dialog box based on the given template id
-/// and error code.
-void __fastcall appfat_dialog_box(template_id template_id, uint32_t error_code, char *log_file_path, int log_line_nr);
+/// ErrOkDlg displays an error dialog box based on the given template id and
+/// error code.
+void __fastcall ErrOkDlg(int template_id, DWORD error_code, char *log_file_path, int log_line_nr);
 
 /// address: 0x401C9C
 ///
-/// appfat_terminate_file_error_dialog terminates the game with a file not found
-/// error dialog.
-void __fastcall appfat_terminate_file_error_dialog(char *error);
+/// FileErrDlg terminates the game with a file not found error dialog.
+void __fastcall FileErrDlg(const char *error);
 
 /// address: 0x401CE1
 ///
-/// appfat_terminate_disk_space_error_dialog terminates the game with an out of
-/// disk space error dialog.
-void __fastcall appfat_terminate_disk_space_error_dialog(char *error);
+/// DiskFreeDlg terminates the game with an out of disk space error dialog.
+void __fastcall DiskFreeDlg(char *error);
 
 /// address: 0x401D1D
 ///
-/// appfat_terminate_insert_cd_error_dialog terminates the game with an insert CD
+/// InsertCDDlg terminates the game with an insert CD
 /// error dialog.
-bool32_t appfat_terminate_insert_cd_error_dialog();
+BOOL InsertCDDlg();
 
 /// address: 0x401D68
 ///
-/// appfat_terminate_directory_error_dialog terminates the game with a read-only
-/// directory error dialog.
-void __fastcall appfat_terminate_directory_error_dialog(char *error);
+/// DirErrorDlg terminates the game with a read-only directory error dialog.
+void __fastcall DirErrorDlg(char *error);
