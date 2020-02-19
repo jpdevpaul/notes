626835927bac6bf5cc60c9098bcb43a0b284e53a
Robin Eklind <rnd0x00@gmail.com>
2020-02-19 10:45:36 +0100

    funcs: improve documentation of encrypt.cpp

--- funcs/encrypt.h
+++ funcs/encrypt.h
@@ -2,48 +2,96 @@
 
 /// address: 0x415F43
 ///
-/// encrypt_decrypt_block decrypts the given block.
-void __fastcall encrypt_decrypt_block(void *block, int size, uint32_t key);
+/// DecryptBlock decrypts the given block.
+///
+/// Parameters:
+///    * block: block of data to decrypt
+///    * size:  size of data
+///    * key:   key used for decryption
+void __fastcall DecryptBlock(DWORD *block, DWORD size, DWORD key);
 
 /// address: 0x415F8F
 ///
-/// encrypt_encrypt_block encrypts the given block.
-void __fastcall encrypt_encrypt_block(void *block, int size, uint32_t key);
+/// EncryptBlock encrypts the given block.
+///
+/// Parameters:
+///    * block: block of data to encrypt
+///    * size:  size of data
+///    * key:   key used for encryption
+void __fastcall EncryptBlock(DWORD *block, DWORD size, DWORD key);
 
 /// address: 0x415FDF
 ///
-/// encrypt_hash returns the hash of the given string.
-uint32_t __fastcall encrypt_hash(char *s, int type);
+/// HashString returns the hash of the given string.
+///
+/// Parameters:
+///    * s:    string to hash
+///    * type: hash algorithm type
+///
+/// Return value:
+///    * ret: 32-bit hash value of the string
+uint32_t __fastcall HashString(const char *s, int type);
 
 /// address: 0x41602E
 ///
-/// encrypt_init_lookup_table initializes the lookup table used for hashing with
-/// pseudo-random numbers based on an LCG with a=125, c=3 and m=0x2AAAAB.
-///
-/// References:
-///    * https://en.wikipedia.org/wiki/Linear_congruential_generator
-void encrypt_init_lookup_table();
+/// InitCryptTable initializes the lookup table used for encryption, decryption
+/// and hashing.
+void InitCryptTable();
 
 /// address: 0x41609D
 ///
-/// encrypt_compress compresses the given buffer using the PKWARE Data
-/// Compression Library.
-int __fastcall encrypt_compress(void *buf, int size);
+/// PkwareCompress compresses the given buffer using the PKWARE Data Compression
+/// Library.
+///
+/// Parameters:
+///    * buf:  buffer to compress (used for both input and output)
+///    * size: size of buffer
+///
+/// Return value:
+///    * ret: size of compressed data
+int __fastcall PkwareCompress(void *buf, int size);
 
+// TODO: uncomment once TDataInfo is defined.
+/*
 /// address: 0x416133
 ///
-/// encrypt_pkware_read reads data from the parameter and writes it to the
+/// PkwareRead reads data from the source buffer, writing to the destination
 /// buffer.
-int __cdecl encrypt_pkware_read(void *buf, int *size, void *param);
+///
+/// Parameters:
+///    * dst:  destination buffer
+///    * size: pointer to number of bytes to read
+///    * info: information used by the PKWARE Data Compression Library (src)
+///
+/// Return value:
+///    * ret: number of bytes read
+int __cdecl PkwareRead(void *dst, int *size, TDataInfo *info);
+*/
 
+// TODO: uncomment once TDataInfo is defined.
+/*
 /// address: 0x416167
 ///
-/// encrypt_pkware_write reads data from the buffer and writes it to the given
-/// parameter.
-int __cdecl encrypt_pkware_write(void *buf, int *size, void *param);
+/// PkwareWrite writes data to the destination buffer, reading from the source
+/// buffer.
+///
+/// Parameters:
+///    * src:  source buffer
+///    * size: pointer to number of bytes to write
+///    * info: information used by the PKWARE Data Compression Library (dst)
+///
+/// Return value:
+///    * ret: number of bytes written
+int __cdecl PkwareWrite(void *src, int *size, TDataInfo *info);
+*/
 
 /// address: 0x41618E
 ///
-/// encrypt_compress decompresses the given buffer using the PKWARE Data
+/// PkwareDecompress decompresses the given buffer using the PKWARE Data
 /// Compression Library.
-void __fastcall encrypt_decompress(void *param, int a2, int size);
+///
+/// Parameters:
+///    * buf:        buffer to decompress
+///    * recv_size:  maximum number of bytes to receive per read operation
+///    * dwMaxBytes: maximum size of decompressed data
+void __fastcall PkwareDecompress(void *buf, int recv_size, int dwMaxBytes);
